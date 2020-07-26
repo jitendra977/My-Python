8829d12fa3fd77423a5b221750bf4bac48d73811
jitendra977 <jitendrakhadka4@gmail.com>
2020-07-26 19:10:38 +0900

    hi

---
+++ map.py
@@ -0,0 +1,9 @@
+# def myfunc(a, b):
+#   return a + b
+sub = ("nepali", "english", "math", "health", "population", "social")
+marks = ("60", "40", "52", "45", "45", "66")
+reasult = map(lambda m, n: m+n, sub, marks)
+
+print(list(reasult))
+
+
